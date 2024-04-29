999c141270b5a08922c62eebb088e4d953ba5b80
Bill <billyen2012@gmail.com>
2024-04-29 19:56:21 +0800

    update dev env

--- src/local-dev.js
+++ src/local-dev.js
@@ -1,19 +1,35 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/bundle.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'dist', 'bundle.js'), {
+app.get('/tw-better-select', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tw-better-select.js'), {
         headers: {
             'Content-Type': 'text/javascript; charset=UTF-8',
         },
-    })
-})
+    });
+});
+
+app.get('/tw-dict', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tw-dict.js'), {
+        headers: {
+            'Content-Type': 'text/javascript; charset=UTF-8',
+        },
+    });
+});
+
+app.get('/constant/TW_BETTER_SELECT', (req, res) => {
+    res.sendFile(path.join(__dirname, 'constant', 'TW_BETTER_SELECT.js'), {
+        headers: {
+            'Content-Type': 'text/javascript; charset=UTF-8',
+        },
+    });
+});
 
 app.listen(3000, () => {
-    console.log('app listening on port 3000')
-})
+    console.log('app listening on port 3000');
+});
